48710efcaa35cdf89fe4672183a1be4b48df341b
SayliV <125791400+SayliV@users.noreply.github.com>
2023-04-26 20:47:50 +0800

    Add files via upload

---
+++ 32.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    char str[100];
+    cout << "Введите строку: ";
+    cin >> str;
+
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        str[i]++;
+    }
+
+    cout << "Получившееся строка: " << str << std::endl;
+
+    return 0;
+}
